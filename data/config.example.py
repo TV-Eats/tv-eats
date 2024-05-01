91a0a1e80c0112a9ae3ca27619145f74600b27d0
Chris Maria <chris@cjmaria.com>
2024-05-01 13:12:31 -0400

    Add data folder with processing scripts

---
+++ data/config.example.py
@@ -0,0 +1,10 @@
+# This is an example of the config file that is used by the scripts in the data folder.
+# Complete the fields you require and rename the file to config.py
+
+DB_USER = ''
+DB_PASSWORD = ''
+DB_HOST = ''
+DB_PORT = ''
+DB_NAME = ''
+
+GOOGLE_MAPS_API_CREDENTIALS_JSON_PATH = ''
