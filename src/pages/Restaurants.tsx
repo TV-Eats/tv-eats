ed879530c9765a2fe0d982e84a561bc506d7e0fa
Chris Maria <chris@cjmaria.com>
2024-06-20 20:49:22 -0400

    Correct search completions and hybrid path param and realtime search

--- src/pages/Restaurants.tsx
+++ src/pages/Restaurants.tsx
@@ -12,137 +12,119 @@ const supabase = createClient<Database>("https://lbcmncanrrxxpnxgqjuy.supabase.c
 
 
 function Restaurants() {
-    const [restaurants, setRestaurants] = useState([] as any[]);
-    const [shows, setShows] = useState([] as any[])
-    const [shows_restaurants, setShows_restaurants] = useState([] as any[]);
-    const [searchParams] = useSearchParams();
-    const [searchBarText, setSearchBarText] = useState('' as string);
-    const [searchBarQuery, setSearchBarQuery] = useState('' as string);
-    let val = searchParams.get('show_id') as unknown as number;
-    let homeSearch = searchParams.get("filter") as unknown as string;
-    let query = searchParams.get("query") as unknown as string; 
+  const [restaurants, setRestaurants] = useState([] as any[]);
+  const [shows_restaurants, setShows_restaurants] = useState([] as any[]);
+  const [searchParams] = useSearchParams();
+  const [searchBarText, setSearchBarText] = useState('' as string);
+  const [searchBarQuery, setSearchBarQuery] = useState('' as string);
+  // let showId = searchParams.get('show_id') as unknown as number;
+  let filterOn = searchParams.get("filter") as unknown as string;
+  let urlQuery = searchParams.get("query") as unknown as string;
 
 
-    async function getRestaurants() {
-        let { data: restaurants, error } = await supabase
-            .from('restaurants')
-            .select()
-        if (error) {
-            console.log('error', error)
-        } else if (restaurants) {
-            setRestaurants(restaurants)
-        }
+  async function getAllRestaurants() {
+    let { data: restaurants, error } = await supabase
+      .from('restaurants')
+      .select()
+    if (error) {
+      console.log('error', error)
+    } else if (restaurants) {
+      setRestaurants(restaurants)
+    }
+  }
+
+  const setSearchBarQueryDebounced = useCallback(
+    debounce((query: string) => { setSearchBarQuery(query) }, 300),
+    []
+  )
+
+  async function searchRestaurantsByName(query: string) {
+    let transformedQuery = `${query.replaceAll(' ', '+')}:*`
+    console.log('Searching by name w. query: ', transformedQuery)
+    let { data: restaurants, error } = await supabase.from('restaurants').select().textSearch('name', transformedQuery)
+    if (error) {
+      console.log('error', error)
+    } else if (restaurants) {
+      setRestaurants(restaurants)
+    }
+  }
+
+  async function searchRestaurantsByCity(query: string) {
+    let transformedQuery = `${query.replaceAll(' ', '+')}:*`
+    console.log('Searching by city w. query: ', transformedQuery)
+    let { data: restaurants, error } = await supabase.from('restaurants').select().textSearch('city', transformedQuery)
+    if (error) {
+      console.log('error searching by city', error)
+    } else if (restaurants) {
+      console.log(`Found ${restaurants.length} restaurants in ${query}. Setting restaurants...`)
+      setRestaurants(restaurants)
+    }
+  }
+
+
+  const handleSearchBarChange = (e: any) => {
+    setSearchBarText(e.target.value);
+    setSearchBarQueryDebounced(e.target.value);
+  }
+
+
+  useEffect(() => {
+    console.log('searchBarQuery UseEffect! called with query: ', searchBarQuery)
+    console.log('filterOn: ', filterOn)
+    if (!urlQuery && searchBarQuery == '') {
+      getAllRestaurants();
+    } else {
+      if (filterOn && filterOn == "Restaurants") {
+        searchRestaurantsByName(searchBarQuery)
+      } else if (filterOn && filterOn == "Cities") {
+        searchRestaurantsByCity(searchBarQuery);
+      } else if (searchBarQuery != '') {
+        searchRestaurantsByName(searchBarQuery)
+      }
     }
 
-    const setSearchBarQueryDebounced = useCallback(
-        debounce((query: string) => {setSearchBarQuery(query)}, 300),
-        []
-    )
+  }, [searchBarQuery])
 
-    async function searchRestaurantsByName(query: string) {
-        console.log('Searching!')
-        let { data: restaurants, error } = await supabase.from('restaurants').select().textSearch('name', query)
-        if (error) {
-            console.log('error', error)
-        } else if (restaurants) {
-            setRestaurants(restaurants)
-        }
+  useEffect(() => {
+    console.log('URL Query: ', urlQuery)
+    if (urlQuery) {
+      setSearchBarQueryDebounced(urlQuery)
+    } else {
+      getAllRestaurants();
     }
 
-    async function searchRestaurantsByCity(query: string) {
-      console.log('Searching!')
-      let { data: restaurants, error } = await supabase.from('restaurants').select().textSearch('city', query)
-      if (error) {
-          console.log('error', error)
-      } else if (restaurants) {
-          setRestaurants(restaurants)
-      }  }
+  }, [])
+
+  useEffect(() => {
+    console.log('Restaurants were set! ', restaurants)
+  }
+    , [restaurants])
 
 
-    const handleSearchBarChange = (e: any) => {
-        setSearchBarText(e.target.value);
-        setSearchBarQueryDebounced(e.target.value);
-    }
-
-
-    useEffect(() => {
-        if (searchBarQuery) {
-            searchRestaurantsByName(searchBarQuery)
-        } else if (homeSearch == "Restaurants") {
-            setSearchBarText(query) ;
-            searchRestaurantsByName(query);
-        } else if (homeSearch == "Cities") {
-          searchRestaurantsByCity(query);  
-          setSearchBarText(query);
-        }
-        
-    }, [searchBarQuery])
-
-    async function getIds() {
-        let { data: shows_restaurants, error } = await supabase
-            .from('shows_restaurants')
-            .select('restaurant_id').eq('show_id', val)
-
-        if (error) {
-            console.log('error', error)
-        } else if (shows_restaurants) {
-            setShows_restaurants(shows_restaurants)
-        }
-    }
-
-    async function getShows() {
-        let { data: shows, error } = await supabase
-            .from('shows')
-            .select("name").eq("id", val)
-        if (error) {
-            console.log('error', error)
-        } else if (shows) {
-            setShows(shows)
-        }
-    }
-
-    useEffect(() => {
-        setSearchBarQuery(query);
-        getRestaurants();
-        getIds();
-        getShows();
-    }, [])
-
-    //console.log(shows_restaurants); // prints out ids of matching restaurants
-    let ids = [] as any;
-    shows_restaurants.forEach((element) => ids.push(element.restaurant_id));
-
-    function isMatch(resId: any) {
-        return ids.includes(resId);
-    }
-
-    let filteredRestaurants = restaurants.filter((restaurant) => isMatch(restaurant.id));
-    console.log(filteredRestaurants);
-
-    return (
-        <div>
-            <center>
-                <span className="text-2xl pt-2.5 inline-flex space-x-2">{val ? filteredRestaurants.length : restaurants.length} restaurants found {query ? <h1> matching "{query}"</h1> : null}</span>
-            </center>
-            <div className="grid grid-cols-1 lg:grid-cols-10 md:grids-cols-2">
-                <div className="lg:col-start-3 lg:col-span-8 md:col-span-1">
-                    <label className="input input-bordered flex items-center gap-2">
-                        <input 
-                            type="text"
-                            className="grow"
-                            placeholder="Search"
-                            value={searchBarText}
-                            onChange={handleSearchBarChange}
-                        />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-                    </label>
-                    <TileLayout tileList={val ? filteredRestaurants : restaurants} arrayType="restaurants" />
-                </div>
-            </div>
+  return (
+    <div>
+      <center>
+        <span className="text-2xl pt-2.5 inline-flex space-x-2">{restaurants.length} restaurants found {searchBarQuery ? <h1> matching "{searchBarQuery}"</h1> : null}</span>
+      </center>
+      <div className="grid grid-cols-1 lg:grid-cols-10 md:grids-cols-2">
+        <div className="lg:col-start-3 lg:col-span-8 md:col-span-1">
+          <label className="input input-bordered flex items-center gap-2">
+            <input
+              type="text"
+              className="grow"
+              placeholder="Search"
+              value={searchBarText}
+              onChange={handleSearchBarChange}
+            />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+          </label>
+          <TileLayout tileList={restaurants} arrayType="restaurants" />
         </div>
+      </div>
+    </div>
 
 
-    );
+  );
 }
 
 export default Restaurants;
